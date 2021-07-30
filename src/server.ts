04d9b8ddd655d6e631acbb8acb1393f0466bd218
Jadir Junior <jadirjsjunior@gmail.com>
2021-07-30 11:43:26 -0300

    iniciando a api node.js com overnightjs e express

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import "./util/module-alias";
+
+import express, { Application } from "express";
+
+import { ForecastController } from "./controllers/forecast";
+import { Server } from "@overnightjs/core";
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
+
+  private setupExpress(): void {
+    // bodyParser depreceted
+    this.app.use(express.json());
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+}
