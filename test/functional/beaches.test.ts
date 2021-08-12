b1abb948face799c9caa43cdeab696635b68960d
Jadir Junior <jadirjsjunior@gmail.com>
2021-08-12 17:14:18 -0300

    express auth middleware e typescript module augmentation

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,7 +1,22 @@
+import AuthService from '@src/services/auth';
 import { Beach } from '@src/models/beach';
+import { User } from '@src/models/user';
 
 describe('Beaches functional tests', () => {
-  beforeAll(async () => await Beach.deleteMany());
+  let token: string;
+
+  beforeEach(async () => {
+    const defaultUser = {
+      name: 'John Doe',
+      email: 'john@mail.com',
+      password: '1234',
+    };
+
+    await Beach.deleteMany();
+    await User.deleteMany();
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
+  });
 
   describe('When creating a new beach', () => {
     it('should create a beach with success', async () => {
@@ -12,7 +27,10 @@ describe('Beaches functional tests', () => {
         position: 'E',
       };
 
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
       expect(response.status).toBe(201);
       // Object containing matches the keys and values, even if includes other keys such as id
       expect(response.body).toEqual(expect.objectContaining(newBeach));
@@ -26,7 +44,10 @@ describe('Beaches functional tests', () => {
         position: 'E',
       };
 
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
         error:
