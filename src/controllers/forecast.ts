5e1223bdc1df5d93cb49081cf5285a50e623c28b
Jadir Junior <jadirjsjunior@gmail.com>
2021-08-30 10:35:37 -0300

    integrando rating e forecast

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -18,7 +18,7 @@ export class ForecastController extends BaseController {
   ): Promise<void> {
     try {
       const beaches = await Beach.find({ user: req.decoded?.id });
-      const forecastData = await forecast.processsForecastForBeaches(beaches);
+      const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
       logger.error(error);
--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -2,6 +2,7 @@ import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
 
 import { Beach } from '@src/models/beach';
 import { InternalError } from '@src/util/errors/internal-error';
+import { Rating } from './rating';
 import logger from '@src/logger';
 
 export interface TimeForeCast {
@@ -18,17 +19,21 @@ export class ForecastProcessingInternetError extends InternalError {
 }
 
 export class Forecast {
-  constructor(protected stormGlass = new StormGlass()) {}
+  constructor(
+    protected stormGlass = new StormGlass(),
+    protected RatingService: typeof Rating = Rating
+  ) {}
 
-  public async processsForecastForBeaches(
+  public async processForecastForBeaches(
     beaches: Beach[]
   ): Promise<TimeForeCast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
     logger.info(`Preparing the forecast for ${beaches.length} beachs`);
     try {
       for (const beach of beaches) {
+        const rating = new this.RatingService(beach);
         const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-        const enrichedBeachData = this.enrichBeachData(points, beach);
+        const enrichedBeachData = this.enrichBeachData(points, beach, rating);
         pointsWithCorrectSources.push(...enrichedBeachData);
       }
       return this.mapForecastByTime(pointsWithCorrectSources);
@@ -56,18 +61,19 @@ export class Forecast {
 
   private enrichBeachData(
     points: ForecastPoint[],
-    beach: Beach
+    beach: Beach,
+    rating: Rating
   ): BeachForecast[] {
-    return points.map((e) => ({
+    return points.map((point) => ({
       ...{},
       ...{
         lat: beach.lat,
         lng: beach.lng,
         name: beach.name,
         position: beach.position,
-        rating: 1, // need to be implemented
+        rating: rating.getRateForPoint(point),
       },
-      ...e,
+      ...point,
     }));
   }
 }
